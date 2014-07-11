85a4a6bb76909908ec892ab3cdae50d53e0fad1f
Tharshan <tharshan09@gmail.com>
2014-07-12 00:40:07 +0100

    Adjust a segment pair in TMX.
    Also comment and clean up code.

--- intellego/term_extraction.py
+++ intellego/term_extraction.py
@@ -14,13 +14,14 @@ def main():
     pairs = zip(tree, tree[1:])[::2]
     corpus = []
     count = 0
-    # iterate through the segment pairs
+    # import the source and target language stopwords
     stopwords_en = nltk.corpus.stopwords.words('english')
     stopwords_es = nltk.corpus.stopwords.words('spanish')
+    # iterate through the segment pairs
     for e,k in pairs: 
         # eliminate any segments that have non alpha terms and terms which are 1 char long
         if (e.text and len(e.text)>1) and (k.text and len(k.text)>1):
-            # split the words of the segment into a list
+            # split the words of the segment into a list. Lowercase all the tokens and elimate any stopwords
             e_token = [w.lower() for w in nltk.word_tokenize(unicode(e.text)) if w.lower() not in stopwords_en]
             k_token = [x.lower() for x in nltk.word_tokenize(unicode(k.text)) if x.lower() not in stopwords_es]
             # add the token list the corpus
@@ -37,7 +38,8 @@ def main():
     print "%s,%s,%s" % ("source", "target", "precision")
     for k,v in model.probabilities.items():
         # print the term pair if the precision is >=0.5 and not the same string
-        if max(v.values()) >= 0.5 and k.encode('utf-8') != max(v, key=v.get).encode('utf-8'):
-            print "%s,%s,%s" % (k.encode('utf-8'),max(v, key=v.get).encode('utf-8'), max(v.values()))
+        if max(v.values()) >= 0.5:
+            if k.encode('utf-8') != max(v, key=v.get).encode('utf-8'):
+                print "%s,%s,%s" % (k.encode('utf-8'),max(v, key=v.get).encode('utf-8'), max(v.values()))
 if __name__ == '__main__':
     main()
