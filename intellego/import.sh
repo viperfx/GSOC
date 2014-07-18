959e5caf3c89049a65a231c6bfb772e063834d87
Tharshan <tharshan09@gmail.com>
2014-07-18 18:33:48 +0100

    Add import script

---
+++ intellego/import.sh
@@ -0,0 +1,5 @@
+yes 1 | amagama-manage dropdb -s en -s es
+amagama-manage initdb -s en -s es
+amagama-manage build_tmdb --verbose -s en -t es -i ../GaiaGlossary.tbx
+amagama-manage build_tmdb --verbose -s en -t es -i logs/term_extraction_segments.csv
+yes 1 | amagama-manage deploy_db
