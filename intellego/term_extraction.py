29243fd591f4bc2e6874ccc07bdae8d14795eb64
Tharshan <tharshan09@gmail.com>
2014-08-08 13:41:03 +0100

    Add NLTK align package locally. Add aligned sent with TUID

--- intellego/term_extraction.py
+++ intellego/term_extraction.py
@@ -7,6 +7,7 @@ from pattern.en import tag as tag_en
 from pattern.es import tag as tag_es
 from pattern.en import pprint as pprint_en
 from pattern.es import pprint as pprint_es
+from align import IBMModel2, AlignedSent
 
 
 def main():
@@ -25,6 +26,7 @@ def main():
     stopwords_es = nltk.corpus.stopwords.words('spanish')
     # iterate through the segment pairs
     for e, k in pairs:
+        # tuid = el.getparent().getparent().attrib['tuid']
         # eliminate any segments that have non alpha terms and terms which are 1 char long
         if (e.text and len(e.text) > 1) and (k.text and len(k.text) > 1):
             # split the words of the segment into a list. Lowercase all the tokens and elimate any stopwords
@@ -36,10 +38,7 @@ def main():
                     # print e.text, e_token, k_token
                     # if 'articles' in e_token:
                         # print e.text, e_token, k_token
-                    corpus.append(nltk.align.AlignedSent(e_token, k_token))
-        # count += 1
-        # if count > 30:
-            # break
+                    corpus.append(AlignedSent(e_token, k_token, tuid=e.getparent().getparent().attrib['tuid']))
     for e, k in pairs:
         # eliminate any segments that have non alpha terms and terms which are 1 char long
         if (e.text and len(e.text) > 1) and (k.text and len(k.text) > 1):
@@ -50,16 +49,19 @@ def main():
             if ''.join(e_token).isalpha() and ''.join(k_token).isalpha():
                 if len(e_token) > 0 and len(k_token) > 0:
                     source_out, target_out = pos_realign(" ".join(e_token), " ".join(k_token))
-                    corpus.append(nltk.align.AlignedSent(source_out, target_out))
+                    corpus.append(AlignedSent(source_out, target_out, tuid=e.getparent().getparent().attrib['tuid']))
     # train the aligned corpus to figure out which pairs of words match
-    model = nltk.align.IBMModel2(corpus, 2)
+    model = IBMModel2(corpus, 1)
     # iterate through the model
-    print "%s,%s,%s" % ("source", "target", "precision")
-    for k, v in model.probabilities.items():
-        # print the term pair if the precision is >=0.5 and not the same string
-        if max(v.values()) >= 0.5:
-            if k.encode('utf-8') != max(v, key=v.get).encode('utf-8'):
-                print "%s,%s,%s" % (k.encode('utf-8'), max(v, key=v.get).encode('utf-8'), max(v.values()))
+    # print "%s,%s,%s" % ("source", "target", "precision")
+    # for k, v in model.probabilities.items():
+    # print the term pair if the precision is >=0.5 and not the same string
+    #     if max(v.values()) >= 0.5:
+    #         if k.encode('utf-8') != max(v, key=v.get).encode('utf-8'):
+    #             print "%s,%s,%s" % (k.encode('utf-8'), max(v, key=v.get).encode('utf-8'), max(v.values()))
+    # get sent alignments with TUID property
+    for sent in corpus:
+        print repr(model.align(sent))
 
 
 def pos_realign(source, target):
@@ -88,4 +90,3 @@ def pos_realign(source, target):
 
 if __name__ == '__main__':
     main()
-    # print pos_realign("Click here to remove all expired articles", "Haga clic aquí para eliminar todos los artículos caducados")
