b59da849a158fffee0d26cb2cda0bc43d60ff107
Tharshan <tharshan09@gmail.com>
2014-08-13 18:08:56 +0100

    Add debug JSON file, add caching, update README

--- README.md
+++ README.md
@@ -2,3 +2,35 @@
 **Mozilla Intellego -- Terminology-driven automatic translation of web sites.**
 
 You can view my progress on the project on this [blog](http://tharshan-gsoc.logdown.com)
+
+##Components
+| Name          | Path          | Description |
+| -------------- | ------------- | ------------- |
+| amaGama Server  | intellego/amagama/ | This contains the [amaGama](http://docs.translatehouse.org/projects/amagama/en/latest/) project with modifications. |
+| Term Extraction Script  | intellego/term_extraction.py | Extracts bilingual terminology from a TMX file. |
+| Import Script | intellego/import.sh | Shell script to delete and recreate the database with data. |
+| Pathfix Script | intellego/pathfix.sh | Script to add the executables in the /bin folder the PATH. |
+| Log files | intellego/logs/ | Various log files created through out the duration of the project. The CSV files contain the results of the term extraction script, which can be immported into the database. |
+| NLTK align | intellego/align |  This contains the NLTK align module with some minor modifications. |
+| Terminology File | memoire_en-US_es-ES.tmx | TMX file downloaded from the [Transvision](http://transvision.mozfr.org/downloads/) project.|
+
+##Terminology Extraction Script
+This script takes a TMX files as an input and can create various outputs based on the commmand line options given. The script uses NLTK and Pattern to analyse the contents of the TMX file to build up a corpus of aligned sentenences. The IBMModel2 in the NLTK Align module is used to build up a model with the aligned corpus. The model contains a mapping of source words to target words with a precision value. The precision value ranges from 0.0 to 1.0, the higher the better.
+```
+Usage: term_extraction.py [OPTIONS]
+
+Options:
+  --load TEXT  Specifiy a model to save.
+  --train      Train the model. If this flag is not set, then it will load a
+               saved model.
+  --debug      Print the debug json file. Pipe the output to a file using this
+               flag.
+  --csv        Print the translated pairs in CSV format. Pipe the output to a
+               file using this flag.
+  --help       Show this message and exit.
+```
+| Log File          | Purpose          |
+| ----------------- | ---------------- |
+| term_extraction_realign2.csv | The latest result of the term extraction tool. |
+| term_extraction_realign.csv | The term extraction result that includes only the POS tagger. |
+| debug_corpus.json | This JSON file contains bilingual terminology with metadata that would help to quickly cross check translations. It includes the TUID from the TMX file for each source word. |
--- intellego/term_extraction.py
+++ intellego/term_extraction.py
@@ -8,60 +8,101 @@ from pattern.es import tag as tag_es
 from pattern.en import pprint as pprint_en
 from pattern.es import pprint as pprint_es
 from align import IBMModel2, AlignedSent
+import json
+import io
+import pdb
+import click
+from cloud.serialization.cloudpickle import dumps, loads
 
 
-def main():
-    # open the tmx file
-    tmx_file = open('../memoire_en-US_es-ES.tmx', 'r')
-    # Parse the TMX file into python objects
-    tmx_tree = etree.parse(tmx_file)
-    # create a list of the "seg" elements, where our segments are contained
-    tree = [e for e in tmx_tree.iter("seg")]
-    # Pair these segments up and put them into a list
-    pairs = zip(tree, tree[1:])[::2]
-    corpus = []
-    count = 0
-    # import the source and target language stopwords
-    stopwords_en = nltk.corpus.stopwords.words('english')
-    stopwords_es = nltk.corpus.stopwords.words('spanish')
-    # iterate through the segment pairs
-    for e, k in pairs:
-        # tuid = el.getparent().getparent().attrib['tuid']
-        # eliminate any segments that have non alpha terms and terms which are 1 char long
-        if (e.text and len(e.text) > 1) and (k.text and len(k.text) > 1):
-            # split the words of the segment into a list. Lowercase all the tokens and elimate any stopwords
-            e_token = [w.lower() for w in nltk.word_tokenize(unicode(e.text)) if w.lower() not in stopwords_en]
-            k_token = [x.lower() for x in nltk.word_tokenize(unicode(k.text)) if x.lower() not in stopwords_es]
-            # add the token list the corpus
-            if ''.join(e_token).isalpha() and ''.join(k_token).isalpha():
-                if len(e_token) > 0 and len(k_token) > 0:
-                    # print e.text, e_token, k_token
-                    # if 'articles' in e_token:
+@click.command()
+@click.option('--load', default='ibm2', help="Specifiy a model to save.")
+@click.option('--train', help="Train the model. If this flag is not set, then it will load a saved model.", is_flag=True)
+@click.option('--debug', help="Print the debug json file. Pipe the output to a file using this flag.", is_flag=True)
+@click.option('--csv', help="Print the translated pairs in CSV format. Pipe the output to a file using this flag.", is_flag=True)
+def main(load, train):
+    if train:
+        # open the tmx file
+        tmx_file = open('../memoire_en-US_es-ES.tmx', 'r')
+        # Parse the TMX file into python objects
+        tmx_tree = etree.parse(tmx_file)
+        # create a list of the "seg" elements, where our segments are contained
+        tree = [e for e in tmx_tree.iter("seg")]
+        # Pair these segments up and put them into a list
+        pairs = zip(tree, tree[1:])[::2]
+        corpus = []
+        count = 0
+        # import the source and target language stopwords
+        stopwords_en = nltk.corpus.stopwords.words('english')
+        stopwords_es = nltk.corpus.stopwords.words('spanish')
+        # iterate through the segment pairs
+        for e, k in pairs:
+            # tuid = el.getparent().getparent().attrib['tuid']
+            # eliminate any segments that have non alpha terms and terms which are 1 char long
+            if (e.text and len(e.text) > 1) and (k.text and len(k.text) > 1):
+                # split the words of the segment into a list. Lowercase all the tokens and elimate any stopwords
+                e_token = [w.lower() for w in nltk.word_tokenize(unicode(e.text)) if w.lower() not in stopwords_en]
+                k_token = [x.lower() for x in nltk.word_tokenize(unicode(k.text)) if x.lower() not in stopwords_es]
+                # add the token list the corpus
+                if ''.join(e_token).isalpha() and ''.join(k_token).isalpha():
+                    if len(e_token) > 0 and len(k_token) > 0:
                         # print e.text, e_token, k_token
-                    corpus.append(AlignedSent(e_token, k_token, tuid=e.getparent().getparent().attrib['tuid']))
-    for e, k in pairs:
-        # eliminate any segments that have non alpha terms and terms which are 1 char long
-        if (e.text and len(e.text) > 1) and (k.text and len(k.text) > 1):
-            # split the words of the segment into a list. Lowercase all the tokens and elimate any stopwords
-            e_token = [w.lower() for w in nltk.word_tokenize(unicode(e.text)) if w.lower() not in stopwords_en]
-            k_token = [x.lower() for x in nltk.word_tokenize(unicode(k.text)) if x.lower() not in stopwords_es]
-            # add the token list the corpus
-            if ''.join(e_token).isalpha() and ''.join(k_token).isalpha():
-                if len(e_token) > 0 and len(k_token) > 0:
-                    source_out, target_out = pos_realign(" ".join(e_token), " ".join(k_token))
-                    corpus.append(AlignedSent(source_out, target_out, tuid=e.getparent().getparent().attrib['tuid']))
-    # train the aligned corpus to figure out which pairs of words match
-    model = IBMModel2(corpus, 1)
-    # iterate through the model
-    # print "%s,%s,%s" % ("source", "target", "precision")
-    # for k, v in model.probabilities.items():
-    # print the term pair if the precision is >=0.5 and not the same string
-    #     if max(v.values()) >= 0.5:
-    #         if k.encode('utf-8') != max(v, key=v.get).encode('utf-8'):
-    #             print "%s,%s,%s" % (k.encode('utf-8'), max(v, key=v.get).encode('utf-8'), max(v.values()))
+                        # if 'articles' in e_token:
+                            # print e.text, e_token, k_token
+                        corpus.append(AlignedSent(e_token, k_token, tuid=e.getparent().getparent().attrib['tuid']))
+
+        for e, k in pairs:
+            # eliminate any segments that have non alpha terms and terms which are 1 char long
+            if (e.text and len(e.text) > 1) and (k.text and len(k.text) > 1):
+                # split the words of the segment into a list. Lowercase all the tokens and elimate any stopwords
+                e_token = [w.lower() for w in nltk.word_tokenize(unicode(e.text)) if w.lower() not in stopwords_en]
+                k_token = [x.lower() for x in nltk.word_tokenize(unicode(k.text)) if x.lower() not in stopwords_es]
+                # add the token list the corpus
+                if ''.join(e_token).isalpha() and ''.join(k_token).isalpha():
+                    if len(e_token) > 0 and len(k_token) > 0:
+                        source_out, target_out = pos_realign(" ".join(e_token), " ".join(k_token))
+                        corpus.append(AlignedSent(source_out, target_out, tuid=e.getparent().getparent().attrib['tuid']))
+
+        # train the aligned corpus to figure out which pairs of words match
+        model = dumps(IBMModel2(corpus, 1))
+        with open("models/ibm2.p", "w") as dest:
+            dest.write(model)
+        result = dumps(corpus)
+        with open("models/corpus", "w") as dest:
+            dest.write(result)
+    else:
+        with open("models/ibm2.p") as source:
+            result = source.read()
+        model = loads(result)
+        with open("models/corpus") as source:
+            result = source.read()
+        corpus = loads(result)
+    if csv:
+        # iterate through the model
+        print "%s,%s,%s" % ("source", "target", "precision")
+        for k, v in model.probabilities.items():
+            # print the term pair if the precision is >= 0.5 and not the same string
+            if max(v.values()) >= 0.5:
+                if k.encode('utf-8') != max(v, key=v.get).encode('utf-8'):
+                    print "%s,%s,%s" % (k.encode('utf-8'), max(v, key=v.get).encode('utf-8'), max(v.values()))
     # get sent alignments with TUID property
-    for sent in corpus:
-        print repr(model.align(sent))
+    if debug:
+        aligned_corpus = {}
+        for sent in corpus:
+            aligned = model.align(sent)
+            for word in aligned.words:
+                v = model.probabilities[word]
+                if word not in aligned_corpus:
+                    if (max(v.values()) >= 0.4) and max(v, key=v.get):
+                        if word.encode('utf-8') != max(v, key=v.get).encode('utf-8'):
+                            aligned_corpus[word] = {}
+                            aligned_corpus[word]['target'] = max(v, key=v.get).encode('utf-8')
+                            aligned_corpus[word]['tuid'] = aligned.tuid
+                            aligned_corpus[word]['precision'] = max(v.values())
+                            aligned_corpus[word]['source_sent'] = " ".join(aligned.words).encode('utf-8')
+                            aligned_corpus[word]['target_sent'] = " ".join(aligned.mots).encode('utf-8')
+                            aligned_corpus[word]['alignment'] = str(aligned.alignment)
+        print json.dumps(aligned_corpus, indent=4, sort_keys=False)
 
 
 def pos_realign(source, target):
